32b2c378848a95e1f9f70e64885e709f3ac77e40
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-10-07 22:56:15 +0530

    Refactor client-side code to integrate socket.io package and implement basic messaging functionality

--- socketIO/client/src/App.jsx
+++ socketIO/client/src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Container, Typography,TextField,Button } from '@mui/material';
+import React, { useEffect,useState } from 'react'
 import {io} from 'socket.io-client'
 const App = () => {
   const socket = io("http://localhost:3000");
+
+  const [message,setMessage] = useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    socket.emit("message",message);
+    setMessage("");
+  }
+  useEffect(()=>{
+    socket.on("connect",()=>{
+      console.log("Connected",socket.id);
+    })
+
+    socket.on("welcome",(data)=>{
+      console.log(data);
+    })
+    return ()=>{
+      socket.disconnect();
+    }
+  },[])
   return (
-    <div>
-      app
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant='h1' component='div' gutterBottom>
+        Welcome to Socket.IO
+      </Typography>
+    <form onSubmit={handleSubmit}>
+    <TextField
+    value={message}
+    onChange={(e)=>setMessage(e.target.value)}
+    id='outlined-basic' label='Message' variant='outlined'/>
+    <Button type='submit' variant='contained' color='primary'>Send</Button>
+    </form>
+    </Container>
   )
 }
 
--- socketIO/server/app.js
+++ socketIO/server/app.js
@@ -1,21 +1,37 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import {createServer} from 'http';
+
 const app  = express();
 const PORT = 3000;
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["GET","POST"],
+        credentials : true
+    }
+});
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
 io.on("connection",(socket)=>{
-    console.log('A user connected');
-    console.log("ID:",socket.id);
+    console.log("User ID:",socket.id);
+    // socket.broadcast.emit("welcome",`User ${socket.id} has joined the chat`);
+
+    socket.on("message",(data)=>{
+        console.log(data);
+        
+    })
+
+    socket.on("disconnect",()=>{
+        console.log(`User ${socket.id} has disconnected`);
+    })
 })
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
