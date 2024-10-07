cf20dcbd9b6a3a432f2f0f4e62cb1a9886e987ab
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-10-07 23:45:02 +0530

    Authentications and Middlewares on connections

--- socketIO/client/src/App.jsx
+++ socketIO/client/src/App.jsx
@@ -3,7 +3,10 @@ import React, { useEffect,useState,useMemo } from 'react'
 import {io} from 'socket.io-client'
 const App = () => {
   const socket = useMemo(()=>
-    io("http://localhost:3000"),[])
+    io("http://localhost:3000",
+      {withCredentials:true}
+    ),[],
+  )
 
   const [message,setMessage] = useState("");
   const [room,setRoom] = useState("");
--- socketIO/server/app.js
+++ socketIO/server/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import {createServer} from 'http';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
 
 const app  = express();
+const secretKeyJwt = "jjankjnakjdnajndand"
 const PORT = 3000;
 const server = createServer(app);
-
 const io = new Server(server,{
     cors:{
         origin:"http://localhost:5173",
@@ -17,6 +20,29 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+
+app.get('/login',(req,res)=>{
+    jwt.sign({
+        _id: "kadkadkandknadknad"
+    },secretKeyJwt)
+    res.cookie("token",{
+        httpOnly:true,
+        secure:true,
+        sameSite:"none"
+    }).json({
+        message:"Login Successful"
+    })
+})
+//Middleware
+io.use((socket,next)=>{
+    cookieParser()(socket.request,socket.request.res,(err)=>{
+        if(err) return next(err)
+        const token = socket.request.cookies.token;
+    if(!token) return next(new Error("Authentication Error"));
+        
+    next();
+    })
+})
 io.on("connection",(socket)=>{
     console.log("User ID:",socket.id);
     // socket.broadcast.emit("welcome",`User ${socket.id} has joined the chat`);
